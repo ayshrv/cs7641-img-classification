f2c14ebb8123d77ada0702a4b03bfaa2a67b1cae
Ayush Shrivastava <shrivastava.ayush1996@gmail.com>
2019-11-16 18:27:59 -0500

    Increased epochs for 2l NN all

--- scripts/run_twolayernn-ssl.sh
+++ scripts/run_twolayernn-ssl.sh
@@ -9,7 +9,7 @@ flags="--model twolayernn \
        --train-data-size 4000 \
        --batch-size 64 \
        --ssl-label-generation-batch-size 64 \
-       --epochs 160 \
+       --epochs 240 \
        --data-aug \
        --optimiser adam \
        --learning-rate 0.001 \
